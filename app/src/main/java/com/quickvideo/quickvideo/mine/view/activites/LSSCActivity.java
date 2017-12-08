37ad11b9413300bcfe979bc87213855f0fadfe56
powersen <powersen_619@163.com>
2017-12-08 13:31:52 +0800

    添加了几个监听事件

--- app/src/main/java/com/quickvideo/quickvideo/mine/view/activites/LSSCActivity.java
+++ app/src/main/java/com/quickvideo/quickvideo/mine/view/activites/LSSCActivity.java
@@ -2,21 +2,34 @@ package com.quickvideo.quickvideo.mine.view.activites;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.GridView;
 
 import com.quickvideo.quickvideo.R;
 import com.quickvideo.quickvideo.allbasic.BaseActivity;
+import com.quickvideo.quickvideo.mine.adapter.MineGridAdapter;
+import com.quickvideo.quickvideo.mine.bean.MineBean;
+import com.quickvideo.quickvideo.mine.presenter.MinePresenter;
+import com.quickvideo.quickvideo.mine.view.MineView;
+
+import java.util.List;
+
+import butterknife.BindView;
 
 /**
  * Created by powersen on 2017/12/7.
  * 历史/收藏 界面
  */
 
-public class LSSCActivity extends BaseActivity {
+public class LSSCActivity extends BaseActivity implements MineView {
+    @BindView(R.id.ls_sc_gridview)
+    GridView lsScGridview;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initView();
-
+        MinePresenter presenter = new MinePresenter(this);
+        presenter.getGridDatas();
     }
 
     @Override
@@ -29,6 +42,21 @@ public class LSSCActivity extends BaseActivity {
     public void getIntentData() {
         super.getIntentData();
 
+    }
+
+    @Override
+    public void showOk() {
+
+    }
+
+    @Override
+    public void showError() {
+
+    }
+
+    @Override
+    public void showAdapter(List<MineBean> list) {
+        lsScGridview.setAdapter(new MineGridAdapter(LSSCActivity.this, list));
     }
 
     @Override
